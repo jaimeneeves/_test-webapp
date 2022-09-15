0fb87c7afe94d19773413503195c31d6ed7e4344
Jaime Neves <jaime.neves@reclameaqui.com.br>
2022-09-15 11:48:12 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aplicação React (Deploy Vercel)
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://instagram.com/universojavascript"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Universo JavaScript
         </a>
       </header>
     </div>
